b1abb927e8b90efc7e9fa01ac2979bba38b8cd11
vinodpanicker <vpanicke@gmail.com>
2013-03-10 05:13:12 +0900

    Added Aggregators and config helpers

--- DataStoreStreamer/test/unit/org/thidunnayil/TestResultSetJsonExtractor.java
+++ DataStoreStreamer/test/unit/org/thidunnayil/TestResultSetJsonExtractor.java
@@ -2,14 +2,36 @@ package org.thidunnayil;
 
 import static org.junit.Assert.*;
 
+import org.codehaus.groovy.grails.web.json.JSONArray;
 import org.junit.Test;
 
 public class TestResultSetJsonExtractor {
+	String USER_HOME= System.getProperty("user.home");
 
 	@Test
 	public void testResultSetJsonExtractor() {
-		//todo add test for Column Visitor
-		fail("Not yet implemented");
+		String tablename = "files";
+		String connectionName = "testmysqlconnection";
+		new MySQLConnector(connectionName).openConnection().fetchResultSet(tablename);
+		
+	}
+	
+	@Test
+	public void testResultSetJsonExtractorForFileConnector() {
+		String directory = USER_HOME+"/.m2";
+		String connectionName = "testfileconnection";
+		assertNotNull(new FileSystemConnector(connectionName).openConnection().fetchJson(directory));
+		
+	}
+	
+	@Test
+	public void testResultSetJsonExtractorUsingAggregator() {
+		String directory = USER_HOME+"/.m2";
+		String connectionName = "testfileconnection";
+		JSONArray result = new FileSystemConnector(connectionName).openConnection().fetch(directory);
+		assertNotNull(result);
+		int actual=new FileConnectorTestHelper().getFileCount(directory);
+		assertEquals(actual,result.length());
 	}
 
 }
